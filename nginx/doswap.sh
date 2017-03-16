a0e780c641c06b754a9c2ce93aeb6fa7d631077a
annyhynna <annyhsu23@gmail.com>
2017-03-16 15:05:41 -0700

    Update doswap.sh

--- nginx/doswap.sh
+++ nginx/doswap.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-function killOldContainer {
+function killitif {
      docker ps -a  > /tmp/yy_xx$$
      if grep --quiet $1 /tmp/yy_xx$$
       then
@@ -13,28 +13,27 @@ function containerNotExist {
      docker ps -a  > /tmp/yy_xx$$
      if grep --quiet $1 /tmp/yy_xx$$
         then
-        echo "container $1 already running"
         return 1
      else
-        echo "container $1 not yet running"
         return 0
     fi
 }
 
 if [ "$1" = "new-activity" ]
 then
-    killOldContainer web1
     NAME=web2
     SWAPFILE=swap2
 elif [ "$1" = "activity" ]
 then
-    killOldContainer web2
     NAME=web1
     SWAPFILE=swap1
 fi
 
 if containerNotExist $NAME
 then
+    killitif web1
+    killitif web2
+
     # Run the new container we want to swap to
     echo "building $1"
     docker run -d -P --network ecs189_default --name $NAME $1
@@ -42,8 +41,13 @@ then
     # Run corresponding swap shell script
     echo $SWAPFILE
     docker exec ecs189_proxy_1 /bin/bash /bin/$SWAPFILE.sh
+else
+    echo "container $1 already running"
 fi
 
-./cleanup.sh
-
+EXITEDCONTAINER=$(docker ps -qa --no-trunc --filter "status=exited")
+if [ ! -z "$EXITEDCONTAINER" ]
+then
+    docker rm "$EXITEDCONTAINER"
+fi
 
