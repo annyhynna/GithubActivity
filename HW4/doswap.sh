250e0c9dd7650e7c42207c384db4bb281d809830
annyhynna <annyhsu23@gmail.com>
2017-03-17 02:40:20 -0700

    Fix doswap.sh

--- HW4/doswap.sh
+++ HW4/doswap.sh
@@ -5,19 +5,9 @@ function killitif {
      if grep --quiet $1 /tmp/yy_xx$$
       then
       echo "killing older version of $1"
-      docker kill `docker ps -a | grep $1  | sed -e 's: .*$::'`
+      docker rm -f `docker ps -a | grep $1  | sed -e 's: .*$::'`
     fi
  }
-
-function containerNotExist {
-     docker ps -a  > /tmp/yy_xx$$
-     if grep --quiet $1 /tmp/yy_xx$$
-        then
-        return 1
-     else
-        return 0
-    fi
-}
 
 if [ "$1" = "new-activity" ]
 then
@@ -29,27 +19,20 @@ then
     NAME=web1
     SWAPFILE=swap1
     KILL=web2
-fi
-
-if containerNotExist $NAME
-then
-    # Run the new container we want to swap to
-    echo "building $1"
-    docker run -d -P --network ecs189_default --name $NAME $1
-
-    # Run corresponding swap shell script
-    echo $SWAPFILE
-    docker exec ecs189_proxy_1 /bin/bash /bin/$SWAPFILE.sh
-
-    # Kill the other container
-    killitif $KILL
 else
-    echo "container $1 already running"
+    NAME=$1
 fi
 
-EXITEDCONTAINER=$(docker ps -qa --no-trunc --filter "status=exited")
-if [ ! -z "$EXITEDCONTAINER" ]
-then
-    docker rm "$EXITEDCONTAINER"
-fi
+# Kill the new container if it previously exists
+killitif $NAME
 
+# Run the new container we want to swap to
+echo "building $1"
+docker run -d -P --network ecs189_default --name $NAME $1
+
+# Run corresponding swap shell script
+echo $SWAPFILE
+docker exec ecs189_proxy_1 /bin/bash /bin/$SWAPFILE.sh
+
+# Kill the old container
+killitif $KILL
